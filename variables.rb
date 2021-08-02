5ce47edf02ab69f2f230279928ad98e8746e8da4
Denys Krupenov <dkru84@gmail.com>
2021-08-03 00:02:34 +0300

    added example for learning the variables

---
+++ variables.rb
@@ -0,0 +1,26 @@
+# strings
+name = 'Iliya'
+
+puts 'My name is: ' + name
+puts 'maybe i do not want this name, but my parents decision is ' + name
+
+# integer
+number = 29
+puts "I am born #{number} years ago."
+puts 'I learn ruby for first time'
+puts number ** 2
+
+# float
+x = 2.5
+y = 5.2
+
+puts "i'm working with decimal numbers"
+puts 'this is a sum of folat numbers: '
+puts x + y
+puts "this is divide of folat numbers: "
+puts x / y
+
+# boolean
+bool = true
+bool = false
+puts "Booleanf always have one of two values True or False"
