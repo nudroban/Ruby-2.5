d6fb52efa21592d552587f3a38bd27f2b1ba6640
Denys Krupenov <dkru84@gmail.com>
2021-08-09 21:59:21 +0300

    separete class and main program code

--- problem.rb
+++ problem.rb
@@ -1,39 +1,9 @@
-class List
-  def initialize
-    @list = {}
-  end
+require_relative 'list'
 
-  def apply(number)
-    @list === number ? @list[number] : add_number(number)
-  end
- 
-  def branch_present?(number)
-    @list === number ? true : false
-  end
-
-  def add_number(number)
-    arr = []
-    x = number
-    while x != 1
-      unless branch_present?(x)
-        x % 2 == 0 ? x = x / 2 : x = (3 * x) + 1
-        arr.append(x)
-      end
-    end
-    @list[number] = arr
-  end
-
-  def show
-    @list.keys.each do |el|
-      puts "#{el} #{@list[el]}"
-    end
-  end
-end
-
-list = List.new()
+alist = List.new()
 
 for x in 1..10000 do
-  list.apply(x)
+  alist.apply(x)
 end
 
-list.show
+alist.show
