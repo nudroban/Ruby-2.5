e1d4ad4541552dbba12e6d5d4a219e51146dfc3b
illia <nudrob577@gmail.com>
2021-08-10 22:59:59 +0300

    task_5 done

--- task_5.rb
+++ task_5.rb
@@ -6,3 +6,19 @@
 # - Выполнить проверку пользовательского ввода на ввод корректных данных.
 #
 # Пример вывода: You have readed XX% of book, you will read XXX pages (YY%).
+
+total_pages = 952
+puts "Book have a #{total_pages} pages"
+print 'Enter the number of pages read: '
+pages = gets
+if pages.to_i > 0
+  pages = pages.to_i
+else
+  puts "#{pages} is not Integer"
+  exit
+end
+ 
+procent_pages = pages * 100 / total_pages
+remaning_pages = total_pages - pages
+rem_pages_procent = (total_pages - pages) * 100 / total_pages
+  puts "You have readed #{procent_pages} % of book,you will read #{remaning_pages} pages (#{rem_pages_procent} %)"
