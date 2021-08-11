6b29b1fc5083e7ade891e07d97879aa3dd848fb4
illia <nudrob577@gmail.com>
2021-08-11 22:32:35 +0300

    task_6 done

--- task_6.rb
+++ task_6.rb
@@ -2,3 +2,13 @@
 # чтобы получить 400 гр.изюма?
 # 
 # Вывести решение задачи в консоль
+
+# float
+grape_water = 80
+grape_product = (100 - grape_water) / 100.0
+raisin_water = 15
+raisin_product = 100 - raisin_water
+weight_r_product = 400 * (raisin_product / 100.0)
+weight_g_product = weight_r_product / grape_product
+final_weight = weight_g_product / 1000
+ puts "#{final_weight} kg"
