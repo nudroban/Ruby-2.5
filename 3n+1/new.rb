eebbce1866b0ceddd138e395bd7225a8e9de8270
illia <nudrob577@gmail.com>
2021-09-14 23:29:25 +0300

    + problem_with_0

--- 3n+1/new.rb
+++ 3n+1/new.rb
@@ -16,6 +16,12 @@ end
 
 def cicle(value)
 a = []
+
+  while value <= 0 do
+    puts "Number is not wrong"
+    exit
+  end
+
   while value != 1 do 
     if is_even?(value) 
       value = calc_even(value)
--- 3n+1/problem.rb
+++ 3n+1/problem.rb
@@ -1,7 +1,7 @@
 require_relative 'list'
 
 list = List.new()
-for x in 1..1000 do
+for x in 1..20 do
   list.apply(x)
 end
 
