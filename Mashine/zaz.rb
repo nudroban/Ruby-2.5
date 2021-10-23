2d39ecd5d64e64607390e232ab3ef2e0c73e4b6a
illia <illia@debian-BULLSEYE-live-builder-AMD64>
2021-10-23 18:28:14 +0300

    +class Userinput

--- Mashine/zaz.rb
+++ Mashine/zaz.rb
@@ -1,6 +1,40 @@
 require_relative 'mashine.rb'
 
+class Userinput < Mashine
+  def initialize
+    sit_down
+  end
 
+  def sit_down
+    puts "ВЫБОР ПЕРСОНАЖА: "
+    puts "ВОДИТЕЛЬ-нажмите 1; ПАССАЖИР-нажмите 2: "
+    user = gets
+  
+    if user.to_i == 1
+      driver
+    elsif user.to_i == 2
+      passenger
+      sit_down
+    else 
+      puts "Ты кто еще такой?А-ну вылазь!!!"
+      sit_down
+    end
+  end
+
+def driver
+    puts "Проверить зеркала,пристегнуть ремень,снять ручник"
+    engine
+  end
+
+  def passenger
+    puts "ЭЭЭЙЙЙ!А где водитель"
+  end
+end
+
+user = Userinput.new
 zaz = Mashine.new
-
+zaz.left
+zaz.right
 zaz.braking
+
+
