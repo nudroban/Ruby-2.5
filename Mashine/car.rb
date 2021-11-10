0b55f8e5a42980e10222e1ca88a0d25656383163
nudroban <nudrob557@gmail.com>
2021-11-10 07:57:51 +0200

    name of cars done

--- Mashine/car.rb
+++ Mashine/car.rb
@@ -1,10 +1,14 @@
 require_relative 'mashine.rb'
 
-opel = Mashine.new(1)
-zaz = Mashine.new("zaz")
-vaz = Mashine.new()
-arr = %w[warming_up akselerate go_ahead engine_count summary_engine_count left right braking stopping]
+
+opel = Mashine.new("ОПЕЛЬ")
+zaz = Mashine.new("ЗАЗ")
+vaz = Mashine.new("ВАЗ")
+for x in 1..5
+arr = %w[akselerate go_ahead engine_count summary_engine_count left right braking stopping]
 method = rand(1..arr.length)
 cars = [opel, zaz, vaz]
 value = rand(1..cars.length)
+cars[value - 1].name
 cars[value - 1].send(arr[method - 1])
+end
