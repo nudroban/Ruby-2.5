977688ed077eb062715cf226cb2fb80c44fc640c
nudroban <nudrob557@gmail.com>
2021-11-19 12:25:12 +0200

    class Mashinizer done

---
+++ Mashine/mashinizer.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Mashinizer
+  def initialize; end
+
+  def mashine_name
+    word
+  end
+
+  private
+
+  def letters
+    # (rand(10..35)).to_s(36)
+    @letter = ('a'..'z').to_a[rand(26)]
+    print @letter
+  end
+
+  def word_length
+    rand(2..4)
+  end
+
+  def word
+    word_length.times { |x| letters }
+  end
+
+end
