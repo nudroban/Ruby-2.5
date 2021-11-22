375098ff1981055368d700b9e1efb2221aabb35b
nudroban <nudrob557@gmail.com>
2021-11-22 16:38:57 +0200

    New Autopark for 100 mashines

---
+++ Mashine/new_autopark.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative 'mashine'
+require_relative 'mashinizer'
+
+cars = []
+names = []
+number = []
+
+100.times do
+  name = Mashinizer.mashine_name
+  names.append(name) unless names.include? name
+end
+
+names.each do |car|
+  car = Mashine.new(car)
+  num = Numberizer.number
+  cars.append(car)
+  number.append(num)
+end
+
+arr = %w[akselerate go_ahead left right braking stopping]
+
+20000.times do
+  method = rand(1..arr.length)
+  value = rand(1..cars.length) - 1
+  cars[value].name
+  cars[value].send(arr[method - 1])
+end
+
+cars.each { |car| print car.name, car.engine_count }
+Mashine.summary_engine_count
